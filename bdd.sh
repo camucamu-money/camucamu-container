828f0867d65cb52fb9d80783c121889562db56bb
luctst <lucas.tostee@gmail.com>
2022-02-25 13:45:24 +0100

    Fix error foor bdd.sh

--- bdd.sh
+++ bdd.sh
@@ -2,7 +2,7 @@
 
 getBackup() {
     echo "Retrieving last backup."
-    docker exec --env-file='./.env' -i db bash -c 'mongodump --archive="./tmp/<database>.gz" --gzip --db <database> --uri "$DB_URL" && mongorestore --gzip --nsInclude="<database>.*" --archive="./tmp/<database>.gz"'
+    docker exec --env-file='./.env' -i mongodb1 bash -c 'mongodump --archive="./tmp/camucamu.gz" --gzip --db camucamu --uri "$DB_URL" && mongorestore --gzip --nsInclude="camucamu.*" --archive="./tmp/camucamu.gz"'
 }
 
 getBackup
